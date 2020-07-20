c4ba360d6c8969d8cc97f70b492c8162382fdaad
Richard <r.richard0000@gmail.com>
2020-07-20 17:07:48 -0300

    Simplify seeding
    Add seed-fu gem and organize seeds as fixtures to simplify the
    seeds organzation.

---
+++ db/fixtures/domains.rb
@@ -0,0 +1,10 @@
+# Initial Domains
+Domain.seed(:name,
+  { name: 'Person' },
+  { name: 'Organization' },
+  { name: 'Course' },
+  { name: 'Credential' },
+  { name: 'Employment' },
+  { name: 'Competency' },
+  { name: 'Prog. of Study' }
+)
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Initial Specifications
-[
-  {
-    name: 'T3 Spine / CEDS (Default)',
-    description: 'Stands for Common Education Data Standards'
-  }
-].each do |attrs|
-  Specification.find_or_initialize_by(name: attrs[:name]).update!(attrs)
-end
-
-# Initial Domains
-[
-  { name: 'Person' },
-  { name: 'Organization' },
-  { name: 'Course' },
-  { name: 'Credential' },
-  { name: 'Employment' },
-  { name: 'Competency' },
-  { name: 'Prog. of Study' }
-].each do |attrs|
-  Domain.find_or_initialize_by(name: attrs[:name]).update!(attrs)
-end
+# Fetch all seed files inside "fixtures" folder
+SeedFu.seed
