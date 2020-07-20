25d001555907c29c61e6e05fabe9a7ba069b4255
Richard <r.richard0000@gmail.com>
2020-07-20 15:14:05 -0300

    Add "Start Mapping" view
    - Link the home page button "Start Mapping" with a new view with
    the same name.
    - Populate the view with all the content shared in the mockup, as a form.

---
+++ app/controllers/mappings_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+###
+# @description This controller holds all the actions related to the mappings.
+#   As an example, when a user wants to start making a mapping, it will show
+#   the "Start Mapping" view to the user
+###
+class MappingsController < ApplicationController
+  # Prepare data for the the "Start Mapping" view and redirect to it
+  def index
+    @specifications = Specification.all
+    @domains = Domain.all.sort_by(&:name)
+  end
+end
