dc4de36242592242cb127a7bbe8a139ad872bb52
Richard <r.richard0000@gmail.com>
2020-07-21 11:31:06 -0300

    Protect all routes to be accessed only with authentication
    Use protection for all routes to avoid unauthenticated access.
    IT can be overriden using `skip_before_action :authenticate_user!`.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Protect all routes to be accessible only with authentication
+  # It can be overriden in any controller with `skip_before_action`
+  before_action :authenticate_user!
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  # Let the user see the home page without authentication
+  skip_before_action :authenticate_user!, only: [:home]
+
   def home
     @current_page = t("home.navigation.view_mappings")
   end
