ad8d214db2fb7a9ef38d911a1bae9bd0492281b7
Richard <r.richard0000@gmail.com>
2020-07-22 15:14:48 -0300

    Add admin user for first use
    Use database seeds to add an admin user-ç

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Fetch all seed files inside "fixtures" folder
+# Fetch all seed files inside "fixtures" folder, as initial seeds
 SeedFu.seed
+
+# Let's create an admin user first
+user = User.create!(email: "admin@t3converter.com", password: "t3admin");
+
+# And an admin role
+role = Role.create!(name: "Admin");
+
+# Assing "admin" role to our admin user
+Assignment.create(user: user, role: role);
