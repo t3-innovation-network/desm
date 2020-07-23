d173fcdf72172b0ce1e9c3410da183e955ac6f51
Richard <r.richard0000@gmail.com>
2020-07-23 12:10:27 -0300

    Initial organizations admin styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -22,5 +21,7 @@ import "./src/media.scss";
 import "./src/navbar.scss";
 import "./src/forms.scss";
 import "./src/stepper.scss";
+import "./src/admin.scss";
 import "@fortawesome/fontawesome-free/js/all";
+
 global.toastr = require("toastr");
