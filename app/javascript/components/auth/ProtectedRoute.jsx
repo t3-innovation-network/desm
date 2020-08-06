5c4e54b85d3ce0764acb0df84a9fedc71d1d7e81
Richard <r.richard0000@gmail.com>
2020-08-06 14:56:26 -0300

    Make the Mappings page availabe for non admins
    The mappings first form page should be available for
    logged in users, even regular users. It's not
    necessary to be an administrator.
    
    The 'ProtectedRoute' component was improved to receive
    an extra param called 'allowNonAdmins', which will help
    on deciding the access grant to the route.

--- app/javascript/components/auth/ProtectedRoute.jsx
+++ app/javascript/components/auth/ProtectedRoute.jsx
@@ -1,28 +1,40 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
+const ProtectedRoute = ({
+  component: Component,
+  allowNonAdmins: allowNonAdmins = false,
+  ...rest
+}) => {
   const isLoggedIn = useSelector((state) => state.loggedIn);
   const user = useSelector((state) => state.user);
   const adminRoleName = process.env.ADMIN_ROLE_NAME || "Admin";
 
   return (
-    /// If we have a valid session, go render the requested route
-    isLoggedIn && user.roles !== undefined && user.roles[0].name == adminRoleName ?
+    /// If we have a valid session
+    isLoggedIn &&
+    /// The signed in user has valid roles
+      user.roles !== undefined &&
+    /// We allow this route for non admins OR the signed in user is an admin
+      (allowNonAdmins || user.roles[0].name == adminRoleName) ? (
+
+      /// Go render the route
       <Route
         {...rest}
-        render={props => (
+        render={(props) => (
           <Component
-          /// Destructure all the props passed to join with all the rest
-          {...rest}
-          {...props}
-        />
+            /// Destructure all the props passed to join with all the rest
+            {...rest}
+            {...props}
+          />
         )}
       />
-    /// If we don't have a valid session, redirect to Sign In page
-    : <Redirect to="/sign-in" />
-  )
-}
+    ) : (
+      /// We don't have a valid session, redirect to Sign In page
+      <Redirect to="/sign-in" />
+    )
+  );
+};
 
 export default ProtectedRoute;
