1fcecfadb20b98621d6dec351dd60f716bf32e75
Alex Nizamov <nizamov.alex@gmail.com>
2020-11-23 15:35:26 +0500

    Fix session store's domain
    Also, use single env variable for storing API host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["BASE_API_URL"] || "http://localhost:3000"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  end
-
-  allow do
-    origins "https://desm.production.com"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    origins ENV["API_URL"]
+    resource "*",
+             credentials: true,
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
