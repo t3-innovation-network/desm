3224534a8b976390e80d8bcc66da96690b96a22f
Richard <r.richard0000@gmail.com>
2021-02-03 11:43:30 -0300

    Fix specs

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -5,7 +5,7 @@ require "faker"
 # Factory for the Organization class
 FactoryBot.define do
   factory :organization do
-    sequence(:id) {|number| number }
+    id { Organization.maximum(:id) + 1 }
     name { Faker::Company.name }
     email { Faker::Internet.email }
   end
