3daccdb15f6a32736b403c0a245218ad14c2e8c4
Ricardo Gamarra <r.richard0000@gmail.com>
2021-09-08 18:21:09 -0300

    Enable mail specs dependant on Github Actions secrets

--- spec/mailers/mapping_spec.rb
+++ spec/mailers/mapping_spec.rb
@@ -8,17 +8,13 @@ RSpec.describe MappingMailer, type: :mailer do
     let(:user) { FactoryBot.build(:user) }
     let(:mail) { MappingMailer.with(mapping: mapping, user: user).updated }
 
-    # @todo: Enable when the GitHub action is configured to mail with secrets (email & password for
-    #   the sender account and Mailgun api key)
-    xit "renders the headers" do
+    it "renders the headers" do
       expect(mail.subject).to eq(I18n.t("mailers.mapping_updated.subject"))
       expect(mail.to).to eq([user.email])
       expect(mail.from).to eq([Rails.configuration.action_mailer.default_options[:from]])
     end
 
-    # @todo: Enable when the GitHub action is configured to mail with secrets (email & password for
-    #   the sender account and Mailgun api key)
-    xit "renders the body" do
+    it "renders the body" do
       expect(mail.body.encoded).to match(I18n.t("mailers.mapping_updated.title"))
     end
   end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,20 +2,18 @@
 
 require "rails_helper"
 
-# @todo: Enable when the GitHub action is configured to mail with secrets (email & password for
-#   the sender account and Mailgun api key)
 RSpec.describe UserMailer, type: :mailer do
   describe "welcome" do
     let(:user) { FactoryBot.build(:user) }
     let(:mail) { UserMailer.with(user: user).welcome }
 
-    xit "renders the headers" do
+    it "renders the headers" do
       expect(mail.subject).to eq(I18n.t("mailers.welcome.subject"))
       expect(mail.to).to eq([user.email])
       expect(mail.from).to eq([Rails.configuration.action_mailer.default_options[:from]])
     end
 
-    xit "renders the body" do
+    it "renders the body" do
       expect(mail.body.encoded).to match(I18n.t("mailers.welcome.title"))
     end
   end
@@ -24,13 +22,13 @@ RSpec.describe UserMailer, type: :mailer do
     let(:user) { FactoryBot.build(:user) }
     let(:mail) { UserMailer.with(user: user).forgot_pass }
 
-    xit "renders the headers" do
+    it "renders the headers" do
       expect(mail.subject).to eq(I18n.t("mailers.forgot_pass.subject"))
       expect(mail.to).to eq([user.email])
       expect(mail.from).to eq([Rails.configuration.action_mailer.default_options[:from]])
     end
 
-    xit "renders the body" do
+    it "renders the body" do
       expect(mail.body.encoded).to match(I18n.t("mailers.forgot_pass.title"))
     end
   end
