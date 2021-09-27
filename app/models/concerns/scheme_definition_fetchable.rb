495ad5502da24e3591e62cabe28eac434561f5a6
Ricardo Gamarra <r.richard0000@gmail.com>
2021-09-27 17:08:25 -0300

    TRy to infer the file extension from HTTP response

--- app/models/concerns/scheme_definition_fetchable.rb
+++ app/models/concerns/scheme_definition_fetchable.rb
@@ -3,12 +3,32 @@
 require "open-uri"
 
 module SchemeDefinitionFetchable
+  EXTENSIONS = {
+    "application/rdf+xml": ".rdf",
+    "application/json": ".json",
+    "text/csv": ".csv",
+    "application/zip": ".zip",
+    "application/xml": ".xml",
+    "text/xml": ".xml",
+  }
+
   def fetch_definition uri
     file_content = URI.open(uri).read
-    file = Tempfile.new(["tmpschemfile", File.extname(uri)])
+    ext = infer_extension(uri)
+    file = Tempfile.new(["tmpschemfile", infer_extension(uri)])
     file.write(file_content)
     file.rewind
 
     Parsers::FormatConverter.convert_to_jsonld(file)
+  end
+
+  private
+
+  def infer_extension uri
+    ext = File.extname(uri)
+    return ext if ext.present?
+
+    response = HTTParty.get(uri)
+    EXTENSIONS[response.headers.content_type.to_sym]
   end
 end
