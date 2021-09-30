ffab97755f1f8db0c2544cf479d38f8de2c93cbf
Ricardo Gamarra <r.richard0000@gmail.com>
2021-09-30 15:21:15 -0300

    Design Dso info tabs
    The Dso Info step of the edit configuration profile view has more information than the others.
    It not only contains metadata about the configuration profile, but also includes agents and schema
    files and concept schemes information.
    
    As in the mockups, the information is presented in this view in a tab system. This commit includes
    the metadata view as the first tab and prepares to add the rest of the tabs in a future commit.

--- app/javascript/components/dashboard/configuration-profiles/edit/DSOInfoWrapper.jsx
+++ app/javascript/components/dashboard/configuration-profiles/edit/DSOInfoWrapper.jsx
@@ -1,13 +1,86 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useSelector } from "react-redux";
 import DSOMetaData from "./DSOMetadata";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfo, faUsers, faFile } from "@fortawesome/free-solid-svg-icons";
 
 const DSOInfoWrapper = () => {
   const currentCP = useSelector((state) => state.currentCP);
   const currentDsoIndex = useSelector((state) => state.currentDSOIndex);
   const getDsos = () => currentCP.structure.standardsOrganizations || [];
+  const [currentTab, setCurrentTab] = useState(0);
+  const tabStyle = { height: "30px", maxWidth: "30px" };
+  const activeTabClass = "bg-dashboard-background col-background";
+  const inactiveTabClass = "border-color-dashboard-dark col-dashboard";
 
-  return <DSOMetaData dsoData={getDsos()[currentDsoIndex]} />;
+  const tabIcon = (index, icon, text) => {
+    return (
+      <div
+        className={`col-1 rounded-circle cursor-pointer ${
+          index === currentTab ? activeTabClass : inactiveTabClass
+        } p-1 text-center`}
+        style={tabStyle}
+        onClick={() => setCurrentTab(index)}
+      >
+        <FontAwesomeIcon
+          icon={icon}
+          data-toggle="tooltip"
+          data-placement="bottom"
+          title={text}
+        />
+      </div>
+    );
+  };
+
+  const line = () => {
+    return (
+      <div className="col-4 border-bottom" style={{ bottom: "1rem" }}></div>
+    );
+  };
+
+  const dsoInfoTabs = () => {
+    return (
+      <Fragment>
+        <div className="row mt-5 justify-content-center">
+          {tabIcon(0, faInfo, "DSO Basic Info")}
+          {line()}
+          {tabIcon(1, faUsers, "Agents")}
+          {line()}
+          {tabIcon(2, faFile, "Schema Files")}
+        </div>
+      </Fragment>
+    );
+  };
+
+  const renderTab = () => {
+    const dsoMetaData = <DSOMetaData dsoData={getDsos()[currentDsoIndex]} />;
+
+    switch (currentTab) {
+      case 0:
+        return dsoMetaData;
+      case 1:
+        return (
+          <div className="row mt-5 justify-content-center">
+            <h3>Agents</h3>
+          </div>
+        );
+      case 2:
+        return (
+          <div className="row mt-5 justify-content-center">
+            <h3>Schema Files</h3>
+          </div>
+        );
+      default:
+        dsoMetaData;
+    }
+  };
+
+  return (
+    <div className="col">
+      {dsoInfoTabs()}
+      {renderTab()}
+    </div>
+  );
 };
 
 export default DSOInfoWrapper;
