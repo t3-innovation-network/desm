8fb09f52f1df3fdc857b41190ffe356a0d30e8aa
Alex Nizamov <nizamov.alex@gmail.com>
2022-07-21 13:31:56 +0500

    List all spines from user's organization on View Mappings page

--- app/controllers/api/v1/spine_specifications_controller.rb
+++ app/controllers/api/v1/spine_specifications_controller.rb
@@ -5,15 +5,12 @@
 ###
 class Api::V1::SpineSpecificationsController < ApplicationController
   before_action :authorize_with_policy
-  before_action :instantiate_spines, only: :index
 
   ###
   # @description: Returns a filtered list of specifications for an organization
   ###
   def index
-    filter
-
-    render json: @spines
+    render json: current_user.organization.spines
   end
 
   private
@@ -23,28 +20,5 @@ class Api::V1::SpineSpecificationsController < ApplicationController
   ###
   def authorize_with_policy
     authorize(with_instance(Specification))
-  end
-
-  ###
-  # @description: Get the list of spine specifications for the user's organization
-  # @return [Array]:
-  ###
-  def instantiate_spines
-    @spines = Domain.all.filter_map {|d| d.spine if d.spine? }
-  end
-
-  ###
-  # @description: Applies the filter/s from the params
-  # @return [Array]
-  ###
-  def filter
-    # Filter by current user if requested
-    unless params[:filter].eql?("all")
-      @spines = @spines.filter_map {|s|
-        s if s.mappings.any? {|m| m&.user.eql?(current_user) }
-      }
-    end
-
-    @spines.sort_by(&:name)
   end
 end
