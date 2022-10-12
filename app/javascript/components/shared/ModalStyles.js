9cc68222decbcdbc4a592898a37ba0d9e17ff4e4
Alex Nizamov <nizamov.alex@gmail.com>
2022-10-12 16:27:24 +0500

    Restyle modal dialogs to fit content better

---
+++ app/javascript/components/shared/ModalStyles.js
@@ -0,0 +1,28 @@
+const ModalStyles = {
+  content: {
+    border: "none",
+    bottom: "auto",
+    left: "50%",
+    marginRight: "-50%",
+    minWidth: "60%",
+    padding: "0",
+    right: "auto",
+    top: "40px",
+    transform: "translate(-50%)",
+    width: "auto"
+  },
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, .8)",
+    display: "flex",
+    height: "100%",
+    justifyContent: "center",
+    left: "0",
+    overflowY: "auto",
+    position: "fixed",
+    top: "0",
+    width: "100%",
+    zIndex: "1000"
+  }
+};
+
+export default ModalStyles;
