23a3ac4ef1e88ee8dc44fcecb792bc372e5a9b72
Alex Nizamov <nizamov.alex@gmail.com>
2023-02-02 17:16:01 +0500

    Exclude empty entities from mapping export

--- app/services/exporters/mapping.rb
+++ app/services/exporters/mapping.rb
@@ -38,9 +38,10 @@ module Exporters
     def export
       {
         "@context": CONTEXT,
-        "@graph": @instance.alignments.map {|alignment|
-          term_nodes(alignment)
-        }.flatten.unshift(main_node)
+        "@graph": [
+          main_node,
+          *term_nodes.compact
+        ]
       }
     end
 
@@ -69,46 +70,67 @@ module Exporters
     #   alignment, one for the mapped property (more than one if there are many), and the last one
     #   for the spine property.
     ###
-    def term_nodes alignment
-      [
-        alignment_node(alignment),
-        alignment.mapped_terms.map {|term| property_node(term) },
-        property_node(alignment.spine_term)
-      ].flatten
+    def term_nodes
+      alignments = @instance
+                   .alignments
+                   .includes(:predicate, mapped_terms: :property, spine_term: :property)
+
+      alignments.map do |alignment|
+        alignment_node = build_alignment_node(alignment)
+        next unless alignment_node
+
+        [
+          alignment_node,
+          *alignment.mapped_terms.map {|term| build_property_node(term) },
+          build_property_node(alignment.spine_term)
+        ]
+      end
     end
 
     ###
     # @description: Specifies the format the alignment node should have.
     ###
-    def alignment_node alignment
-      {
+    # rubocop:disable Metrics/AbcSize
+    def build_alignment_node alignment
+      return if alignment.predicate.nil? || alignment.spine_term.nil?
+
+      mapped_terms = alignment.mapped_terms.map {|mapped_term|
+        {"@id": mapped_term.uri} if mapped_term.uri.nil?
+      }
+
+      node = {
         "@id": "http://desmsolutions.org/TermMapping/#{alignment.id}",
         "@type": "desm:TermMapping",
         "dcterms:isPartOf": {"@id": "http://desmsolutions.org/TermMapping/#{@instance.id}"},
-        "desm:comment": alignment.comment,
-        "desm:mappedterm": alignment.mapped_terms.map {|mapped_term|
-          {"@id": mapped_term.uri}
-        },
-        "desm:mappingPredicate": {"@id": alignment.predicate&.uri},
+        "desm:mappedterm": mapped_terms.compact,
+        "desm:mappingPredicate": {"@id": alignment.predicate.uri},
         "desm:spineTerm": {"@id": alignment.spine_term.uri}
       }
+
+      node["desm:comment"] = alignment.comment if alignment.comment?
+      node
     end
 
     ###
     # @description: Defines the structure of a generic property term.
     ###
-    def property_node term
-      {
+    def build_property_node term
+      property = term.property
+
+      node = {
         "@id": term.source_uri,
         "@type": "rdf:Property",
-        "desm:sourceURI": {"@id": term.property.source_uri},
-        "rdfs:subPropertyOf": {"@id": term.property.subproperty_of},
-        "desm:valueSpace": {"@id": term.property.value_space},
-        "rdfs:label": term.property.label,
-        "rdfs:comment": term.property.comment,
-        "rdfs:domain": {"@id": term.property.selected_domain},
-        "rdfs:range": {"@id": term.property.selected_range}
+        "desm:sourceURI": {"@id": property.source_uri},
+        "rdfs:label": term.property.label
       }
+
+      node["rdfs:comment"] = property.comment if property.comment?
+      node["rdfs:domain"] = {"@id": property.selected_domain} if property.selected_domain?
+      node["rdfs:range"] = {"@id": property.selected_range} if property.selected_range?
+      node["rdfs:subPropertyOf"] = {"@id": property.subproperty_of} if property.subproperty_of?
+      node["desm:valueSpace"] = {"@id": property.value_space} if property.value_space?
+      node
     end
+    # rubocop:enable Metrics/AbcSize
   end
 end
