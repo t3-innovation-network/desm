e3150ef4b7db6d5313f122682e9ca59eb9255dbb
Oksana Melnikova <oksana.melnikova@gmail.com>
2024-03-09 23:00:30 +0400

    (#472) prevented removal term with mappings

--- app/javascript/services/fetchSpineTerms.jsx
+++ app/javascript/services/fetchSpineTerms.jsx
@@ -1,12 +1,13 @@
 import { camelizeKeys } from 'humps';
 import apiRequest from './api/apiRequest';
 
-const fetchSpineTerms = async (specId) => {
+const fetchSpineTerms = async (specId, queryParams = {}) => {
   const response = await apiRequest({
     url: '/api/v1/spines/' + specId + '/terms',
     defaultResponse: [],
     successResponse: 'terms',
     method: 'get',
+    queryParams,
   });
   return camelizeKeys(response);
 };
